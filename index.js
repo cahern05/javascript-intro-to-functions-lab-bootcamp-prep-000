7ad088400555599cde883352e12093203c299b1f
Colin Ahern <cahern05@gmail.com>
2018-01-25 19:51:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,16 +16,10 @@ function logWhisper(string)
 }
 function sayHiToGrandma(string)
 {
-  if (string = string.toLowerCase());
+  if (string = string.toLowerCase())
     console.log("I can't hear you!");
-  else if 
-
-function sayHiToGrandma(string)
-{ 
-  if (string = string.toUpperCase());
-    return "YES INDEED!"
-}
-function sayHiToGrandma(string)
-{ if (string = "I love you, Grandma")
-    return "I love you, too."
+  else if (string = string.toUpperCase())
+    return "YES INDEED!";
+  else if (string = "I love you, Grandma")
+    return "I love you, too.";
 }
