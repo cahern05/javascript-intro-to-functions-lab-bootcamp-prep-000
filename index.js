3f196c166ae5c4b39b1f79b74c1107fd1045e92b
Colin Ahern <cahern05@gmail.com>
2018-01-25 19:37:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function logWhisper(string)
 function sayHiToGrandma(string)
 {
   if (string = string.toLowerCase());
-    return "I can't hear you" }
-{ else if (string = string.toUppercase());
+    return "I can't hear you"
+} else if (string = string.toUppercase());
     return "YES INDEED!"
   
   
